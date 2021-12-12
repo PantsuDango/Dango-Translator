7049046dccc92d77455b5d32deada9b029bd446e
PantsuDango <394883561@qq.com>
2021-12-12 16:54:09 +0800

    自动更新说明新增显示屏蔽状态栏效果图

--- utils/update.py
+++ utils/update.py
@@ -6,6 +6,8 @@ import utils.http
 
 OCR_SRC_FILE_PATH = "./ocr/resources/app.py"
 FUNCTION_ICON_PATH = "./config/icon/function.png"
+OPEN_STATUSBAR_IMG_PATH = "./config/other/显示消息栏.png"
+CLOSE_STATUSBAR_IMG_PATH = "./config/other/屏蔽消息栏.png"
 
 
 # 更新本地ocr源码文件
@@ -29,3 +31,11 @@ def updateIcon(yaml, logger) :
     if not os.path.exists(FUNCTION_ICON_PATH) :
         url = yaml["dict_info"]["function_icon_url"]
         utils.http.downloadFile(url, FUNCTION_ICON_PATH, logger)
+
+    if not os.path.exists(OPEN_STATUSBAR_IMG_PATH) :
+        url = yaml["dict_info"]["open_statusbar_url"]
+        utils.http.downloadFile(url, OPEN_STATUSBAR_IMG_PATH, logger)
+
+    if not os.path.exists(CLOSE_STATUSBAR_IMG_PATH) :
+        url = yaml["dict_info"]["close_statusbar_url"]
+        utils.http.downloadFile(url, CLOSE_STATUSBAR_IMG_PATH, logger)
