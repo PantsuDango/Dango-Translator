f1bfa7a98e5697135c1aba805afff3bd6b99ddf3
PantsuDango <394883561@qq.com>
2021-12-12 18:20:23 +0800

    修复不能修改密码的问题

--- ui/register.py
+++ ui/register.py
@@ -429,7 +429,7 @@ class Register(QWidget) :
             if code_key != self.code_key:
                 utils.message.MessageBox("修改失败",
                                          "邮箱验证码错误!     ")
-            return
+                return
 
         url = self.object.yaml["dict_info"]["dango_modify_password"]
         body = {
