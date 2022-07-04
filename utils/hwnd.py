c7235b865d3bb4e9750ccb4bd3595ef427d6afa0
Dango <394883561@qq.com>
2022-07-04 14:05:53 +0800

    开发全屏下置顶且可操作性

--- utils/hwnd.py
+++ utils/hwnd.py
@@ -7,27 +7,36 @@ from win32.lib import win32con
 
 # 全屏下置顶
 def setWindowTop(object) :
-    # 翻译界面句柄
-    object.translation_ui_hwnd = int(object.translation_ui.winId())
     # 桌面分辨率
     rect_desk = win32gui.GetWindowRect(win32gui.GetDesktopWindow())
+    # 翻译界面句柄
+    object.translation_ui_hwnd = int(object.translation_ui.winId())
+    # 设置界面句柄
+    object.settin_ui_hwnd = int(object.settin_ui.winId())
+    # 范围界面句柄
+    object.range_ui_hwnd = int(object.range_ui.winId())
+
+
     while True :
         time.sleep(3)
         # 判断当前激活窗体是否为全屏
-        sign, hwnd_now = checkIsFullScreen(rect_desk)
+        sign, hwnd = checkIsFullScreen(rect_desk)
         if not sign :
             continue
-        # 翻译界面无焦点
-        win32gui.SetWindowLong(object.translation_ui_hwnd, win32con.GWL_EXSTYLE, win32con.WS_EX_NOACTIVATE)
+
+        # 设置窗口置顶且无焦点
+        setTop(object.translation_ui_hwnd)
+        setTop(object.settin_ui_hwnd)
+        setTop(object.range_ui_hwnd)
+
         while True :
-            time.sleep(3)
-            # 翻译界面置顶
-            win32gui.SetWindowPos(object.translation_ui_hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOSIZE | win32con.SWP_NOMOVE)
             # 如果退出全屏程序
-            if win32gui.GetWindowRect(hwnd_now) != rect_desk :
+            time.sleep(3)
+            if win32gui.GetWindowRect(hwnd) != rect_desk :
                 # 恢复翻译界面焦点
-                style = win32gui.GetWindowLong(object.translation_ui_hwnd, win32con.GWL_EXSTYLE)
-                win32gui.SetWindowLong(object.translation_ui_hwnd, win32con.GWL_EXSTYLE, style &~ win32con.WS_EX_NOACTIVATE)
+                releaseFocus(object.translation_ui_hwnd)
+                releaseFocus(object.settin_ui_hwnd)
+                releaseFocus(object.range_ui_hwnd)
                 break
 
 
@@ -42,3 +51,18 @@ def checkIsFullScreen(rect_desk) :
         return False, hwnd
 
     return True, hwnd
+
+
+# 设置窗口置顶且无焦点
+def setTop(hwnd) :
+    # 窗口无焦点
+    win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32con.WS_EX_NOACTIVATE)
+    # 窗口置顶
+    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOSIZE | win32con.SWP_NOMOVE)
+
+
+# 解除窗口焦点
+def releaseFocus(hwnd) :
+    # 恢复窗口焦点
+    style = win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE)
+    win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, style &~ win32con.WS_EX_NOACTIVATE)
