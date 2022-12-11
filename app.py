e7ae3841988e5f56a0cf930273664b5a5278fdc5
Dango <394883561@qq.com>
2022-12-11 10:14:39 +0800

    发布4.3.8

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ class DangoTranslator :
         # 本地配置
         self.yaml = utils.config.openConfig(self.logger)
         # 版本号
-        self.yaml["version"] = "4.3.8-Beta"
+        self.yaml["version"] = "4.3.8"
         # 配置中心
         self.yaml["dict_info"] = utils.config.getDictInfo(self.yaml["dict_info_url"], self.logger)
         # 屏幕分辨率
