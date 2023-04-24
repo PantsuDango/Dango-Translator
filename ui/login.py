1df848a5305bf7de032d761b32b96df940991016
Dango <394883561@qq.com>
2023-04-24 23:41:40 +0800

    1. 修复网络错误会导致翻译器崩溃问题; 2. 发布4.4.3版本

--- ui/login.py
+++ ui/login.py
@@ -160,7 +160,7 @@ class Login(QWidget) :
         # 版本号
         label = QLabel(self)
         self.customSetGeometry(label, 20, 540, 380, 15)
-        label.setText("版本号: %s  更新时间: 2023-03-24  By: 胖次团子"%self.object.yaml["version"])
+        label.setText("版本号: %s  更新时间: 2023-04-24  By: 胖次团子"%self.object.yaml["version"])
         label.setStyleSheet("color: %s;"
                             "background: transparent;"
                             "font-weight:bold;"
