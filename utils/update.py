6e5e774d8b2cc98170b75346d275fced5e755c73
Dango <394883561@qq.com>
2023-04-25 15:52:05 +0800

    1. 修改连接配置中心加入ip直连兜底逻辑; 2. 新增自动更新自动更新程序逻辑; 2. 发布4.4.4版本

--- utils/update.py
+++ utils/update.py
@@ -39,20 +39,19 @@ def updatePilFile(object) :
 # 更新自动更新程序
 def updateAutoUpdateFile(object) :
 
-    if not os.path.exists(AUTO_UPDATE_FILE_PATH) :
-        return
-    auto_update_file_md5 = object.yaml["dict_info"].get("auto_update_file_md5", "")
-    if not auto_update_file_md5 :
-        return
-    # 计算当前自动更新程序的MD5
-    hash_md5 = hashlib.md5()
-    with open(AUTO_UPDATE_FILE_PATH, "rb") as file :
-        for chunk in iter(lambda: file.read(4096), b"") :
-            hash_md5.update(chunk)
-    file_md5 = hash_md5.hexdigest()
-    # 比较MD5, 判断是否需要更新
-    if file_md5 == auto_update_file_md5 :
-        return
+    if os.path.exists(AUTO_UPDATE_FILE_PATH) :
+        auto_update_file_md5 = object.yaml["dict_info"].get("auto_update_file_md5", "")
+        if not auto_update_file_md5 :
+            return
+        # 计算当前自动更新程序的MD5
+        hash_md5 = hashlib.md5()
+        with open(AUTO_UPDATE_FILE_PATH, "rb") as file :
+            for chunk in iter(lambda: file.read(4096), b"") :
+                hash_md5.update(chunk)
+        file_md5 = hash_md5.hexdigest()
+        # 比较MD5, 判断是否需要更新
+        if file_md5 == auto_update_file_md5 :
+            return
     # 下载新的自动更新程序
     url = object.yaml["dict_info"].get("auto_update_file_url", "")
     if not url :
